c125e27ffdd0b568b55a6653285869d481358433
wrzesienjessica <jessica.wrzesien@gmail.com>
2025-04-13 18:24:32 -0400

    made placeholder screens and the tab bar.

---
+++ iOS-Bootcamp-Project/AnalysisScreen.swift
@@ -0,0 +1,7 @@
+//
+//  Analysis.swift
+//  iOS-Bootcamp-Project
+//
+//  Created by Jessica Wrzesien on 4/13/25.
+//
+
---
+++ iOS-Bootcamp-Project/DashboardScreen.swift
@@ -0,0 +1,7 @@
+//
+//  Dashboard.swift
+//  iOS-Bootcamp-Project
+//
+//  Created by Jessica Wrzesien on 4/13/25.
+//
+
