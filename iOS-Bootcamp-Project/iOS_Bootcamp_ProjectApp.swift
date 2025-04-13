ba1e0dfb0068a48ecd3d41ca8ecca61a638918e4
Jason Wrzesien <jason.wrzesien@icloud.com>
2025-04-13 16:05:57 -0400

    firebase update

--- iOS-Bootcamp-Project/iOS_Bootcamp_ProjectApp.swift
+++ iOS-Bootcamp-Project/iOS_Bootcamp_ProjectApp.swift
@@ -8,24 +8,25 @@
 import SwiftUI
 import FirebaseCore
 
-@main
-struct iOS_Bootcamp_ProjectApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-    init() {
-        FirebaseApp.configure()
-    }
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
