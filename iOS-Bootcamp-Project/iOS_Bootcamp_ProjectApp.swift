af491b3c08e2bd070d528c19d10fa61dd1907ca3
Jason Wrzesien <jason.wrzesien@icloud.com>
2025-04-13 15:21:31 -0400

    Initial files

---
+++ iOS-Bootcamp-Project/iOS_Bootcamp_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Bootcamp_ProjectApp.swift
+//  iOS-Bootcamp-Project
+//
+//  Created by Jason Wrzesien on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Bootcamp_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS-Bootcamp-ProjectTests/iOS_Bootcamp_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Bootcamp_ProjectTests.swift
+//  iOS-Bootcamp-ProjectTests
+//
+//  Created by Jason Wrzesien on 4/13/25.
+//
+
+import Testing
+@testable import iOS_Bootcamp_Project
+
+struct iOS_Bootcamp_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
