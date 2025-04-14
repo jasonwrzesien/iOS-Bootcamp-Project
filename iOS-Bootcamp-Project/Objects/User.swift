70645e7be735297e59160324686e26564f95a908
Jason Wrzesien <jason.wrzesien@icloud.com>
2025-04-14 15:06:37 -0400

    initial firebase setup

---
+++ iOS-Bootcamp-Project/Objects/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  iOS-Bootcamp-Project
+//
+//  Created by Jason Wrzesien on 4/14/25.
+//
+
+import Foundation
---
+++ iOS-Bootcamp-Project/Views/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  iOS-Bootcamp-Project
+//
+//  Created by Jason Wrzesien on 4/14/25.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
